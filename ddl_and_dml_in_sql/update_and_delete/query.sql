10f81fd43e221f97d6ac5f86bf856b8c801b0670
thetendie <>
2026-03-28 03:54:26 +0000

    Update and Delete

---
+++ ddl_and_dml_in_sql/update_and_delete/query.sql
@@ -0,0 +1,11 @@
+-- Write your solution here
+UPDATE medications
+SET price = 10
+WHERE  price < 10;
+
+
+
+-- Please do not modify the code below!
+SELECT * FROM medications
+ORDER BY id;
+-- Please do not modify the code above!
