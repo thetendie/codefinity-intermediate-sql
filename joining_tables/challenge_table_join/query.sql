b24842497639416b2500589326585a744d1628b9
thetendie <>
2026-03-28 03:13:36 +0000

    Challenge: Table Join

---
+++ joining_tables/challenge_table_join/query.sql
@@ -0,0 +1,10 @@
+select 
+enrollments.enrollment_id,
+enrollments.student_name,
+enrollments.enrollment_date,
+courses.course_name,
+courses.description
+
+from courses
+
+right join enrollments on enrollments.course_id = courses.course_id
